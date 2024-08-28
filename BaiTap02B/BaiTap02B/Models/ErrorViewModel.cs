f441c7116363db49eaf6e8de1795fcfcd83b4d05
HoazgNam <1822041669@dntu.edu.vn>
2024-08-28 15:13:58 +0700

    hoanthanh bai2B

--- BaiTap02B/BaiTap02B/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BaiTap02B.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
