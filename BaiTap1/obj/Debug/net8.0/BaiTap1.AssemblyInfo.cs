392b0aaf76ce2d4be3fbac817bb9369bd289362d
HoazgNam <1822041669@dntu.edu.vn>
2024-08-28 15:05:14 +0700

    hoan thanh 02B

--- BaiTap1/obj/Debug/net8.0/BaiTap1.AssemblyInfo.cs
+++ BaiTap1/obj/Debug/net8.0/BaiTap1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b913ad034fbd0717cd61c03613a579ad27c1a17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTap1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Baitap2/obj/Debug/net8.0/Baitap2.AssemblyInfo.cs
+++ Baitap2/obj/Debug/net8.0/Baitap2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Baitap2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b913ad034fbd0717cd61c03613a579ad27c1a17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Baitap2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Baitap2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
