c6d081cefa5a7479b81a6ddd6c186f49cd6bb1f1
HoazgNam <1822041669@dntu.edu.vn>
2024-09-06 10:51:20 +0700

    hoanthanhbt

---
+++ BaiTapVeNha02/Controllers/Tuan02Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.HoTen = "Luu Hoang Nam";
+            ViewBag.MSSV = "1822041669";
+            ViewBag.Nam = 2024;
+
+            return View();
+        }
+    }
+}
